c331b27af17dd22055243fd9ec8616eb2f7f95e6
Steven Armstrong <codemumbler@users.noreply.github.com>
2015-09-03 20:47:53 -0400

    replaces text in data-i18n

--- js/reword.js
+++ js/reword.js
@@ -1,8 +1,11 @@
 (function($){
 	$.fn.reword = function(options) {
 		for (key in options) {
-			$('*[data-i18n=' + key + ']').each(function(index, element){
-				$(element).text(options[$(element).data('i18n')]);
+			$(this).find('*[data-i18n=' + key + ']').each(function(index, element){
+				$(element).text(options[key]);
+			});
+			$(this).find('*[data-alt-i18n=' + key + ']').each(function(index, element){
+				$(element).attr('alt', options[key]);
 			});
 		}
 	}
--- test/reword.Spec.js
+++ test/reword.Spec.js
@@ -8,18 +8,27 @@ describe('reword.js', function() {
 
 	describe('reword', function(){
 		beforeEach(function(){
-			$(document.body).append('<div id="testElement" data-i18n="msg1"/>')
+			$(document.body).append('<div class="test-element" data-i18n="msg1"/>')
 		});
 
 		afterEach(function(){
-			$('#testElement').remove();
+			$('.test-element').remove();
 		});
 
 		it('Puts data-i18n with text', function(){
 			$(document.body).reword({
 				'msg1': 'Test message 1'
 			})
-			expect($('#testElement').text()).toEqual('Test message 1');
+			expect($('.test-element').text()).toEqual('Test message 1');
+		});
+
+		it('alt text', function(){
+			$(document.body).append('<img class="test-element" data-alt-i18n="msg2" src=""/>');
+			$(document.body).reword({
+				'msg1': 'Test message 1',
+				'msg2': 'Test alt message'
+			});
+			expect($('img.test-element').attr('alt')).toEqual('Test alt message');
 		});
 	});
 });
