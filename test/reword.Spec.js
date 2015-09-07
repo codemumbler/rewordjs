94a1676fdd60a2fb04644a394ef573f4e4937250
Steven Armstrong <codemumbler@users.noreply.github.com>
2015-09-07 14:45:25 -0400

    test clean up

--- test/reword.Spec.js
+++ test/reword.Spec.js
@@ -1,4 +1,10 @@
 describe('reword.js', function() {
+	var messages = {
+		'msg1': 'Test message 1',
+		'msg2': 'Test message 2',
+		'msg-alt': 'Test alt message'
+	};
+	
 	beforeEach(function() {
 		$(document.body).append('<div class="test-element" data-i18n="msg1"/>')
 	});
@@ -11,49 +17,40 @@ describe('reword.js', function() {
 		it('jquery plugin', function() {
 			expect(typeof $.fn.reword).toBe('function');
 		});
+
+		it('jquery chaining', function() {
+			expect($('.test-element').reword(messages).hasClass('test-element')).toBe(true);
+		});
 	});
 
 	describe('place messages', function() {
 		it('Puts data-i18n with text', function() {
-			$(document.body).reword({
-				'msg1': 'Test message 1'
-			});
+			$(document.body).reword(messages);
 			expect($('.test-element').text()).toEqual('Test message 1');
 		});
 
 		it('alt text', function() {
-			$(document.body).append('<img class="test-element" data-alt-i18n="msg2" src=""/>');
-			$(document.body).reword({
-				'msg1': 'Test message 1',
-				'msg2': 'Test alt message'
-			});
+			$(document.body).append('<img class="test-element" data-alt-i18n="msg-alt" src=""/>');
+			$(document.body).reword(messages);
 			expect($('img.test-element').attr('alt')).toEqual('Test alt message');
 		});
 
 		it('on specific element places text', function() {
-			$('.test-element').reword({
-				'msg1': 'Test message 1'
-			});
+			$('.test-element').reword(messages);
 			expect($('.test-element').text()).toEqual('Test message 1');
 		});
 	});
 
 	describe('place message dynamically', function() {
 		it('changes as data-i18n changes', function() {
-			$(document.body).reword({
-				'msg1': 'Test message 1',
-				'msg2': 'Test message 2'
-			});
+			$(document.body).reword(messages);
 			$('.test-element').data('i18n', 'msg2');
 			expect($('.test-element').data('i18n')).toEqual('msg2');
 			expect($('.test-element').text()).toEqual('Test message 2');
 		});
 
 		it('change to other data-attr does nothing', function() {
-			$(document.body).reword({
-				'msg1': 'Test message 1',
-				'msg2': 'Test message 2'
-			});
+			$(document.body).reword(messages);
 			$('.test-element').data('test', 'msg2');
 			expect($('.test-element').text()).toEqual('Test message 1');
 		});
@@ -66,16 +63,14 @@ describe('reword.js', function() {
 			});
 
 			it('appending html places messages', function(done) {
-				$(document.body).reword({
-					'msg1': 'Test message 1',
-					'msg2': 'Test message 2'
-				}).on('reword', function(event, element) {
-					if ($(element).attr('id') == 'appendedDiv') {
-						expect($('#appendedDiv').text()).toEqual('Test message 2');
-						done();
-						$(document.body).off('reword');
-					}
-				}).append('<div id="appendedDiv" class="async-test-element" data-i18n="msg2"/>');
+				$(document.body).reword(messages)
+					.on('reword', function(event, element) {
+						if ($(element).attr('id') == 'appendedDiv') {
+							expect($('#appendedDiv').text()).toEqual('Test message 2');
+							done();
+							$(document.body).off('reword');
+						}
+					}).append('<div id="appendedDiv" class="async-test-element" data-i18n="msg2"/>');
 			});
 
 			afterEach(function() {
